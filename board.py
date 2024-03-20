4ab51374359892aef3545b630ecba337f3c412f3
Bodi Szabo <bodszab@nuevaschool.org>
2024-03-20 14:35:16 -0700

    Board interface

---
+++ board.py
@@ -0,0 +1,15 @@
+class Board():
+    def __init__(self):
+        self.active_qbit = None
+
+    def step(self):
+        #move qbit probabilities
+        pass
+
+    def measure(self):
+        #collapse wave function for position
+        pass
+
+    def apply_pauli_matrix(self, pauli_matrix):
+        #apply pauli matrix to active qbit
+        pass
