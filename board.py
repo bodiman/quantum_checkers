82ac34df46002496dde29de36ba9335a0d3e157d
tainish <97322839+tainish@users.noreply.github.com>
2024-03-21 20:50:28 -0700

    Update board.py

--- board.py
+++ board.py
@@ -1,10 +1,30 @@
+import math
+
 class Board():
     def __init__(self):
         self.active_qbit = None
+        self.N = 10
+        self.multiplier = [[0, 1, 4, 4, 1],
+                           [1, 2, 5, 5, 2],
+                           [4, 5, 8, 8, 5],
+                           [4, 5, 8, 8, 5],
+                           [1, 2, 5, 5, 2]]
+        for i in range(5):
+            for j in range(5):
+                
+
+        self.q1_prob = [[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+                        [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+                        [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+                        [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+                        [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]]
+        self.q2_prob = self.q1_prob
 
     def step(self):
-        #move qbit probabilities
-        pass
+        for u in range(5):
+            for v in range(5):
+                self.q1_prob[u][v]
+                
 
     def measure(self):
         #collapse wave function for position
